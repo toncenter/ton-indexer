0c8794ae2f7f17b1ccc3c474b2e1585423633fe6
dungeon-master-666 <dungeon666master@protonmail.com>
2022-04-19 01:47:28 +0200

    redis as result backend

--- indexer/celery.py
+++ indexer/celery.py
@@ -5,8 +5,11 @@ from config import settings
 RABBITMQ_HOST = os.getenv("RABBITMQ_HOST")
 RABBITMQ_PORT = os.getenv("RABBITMQ_PORT")
 
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = os.getenv("REDIS_PORT")
+
 CELERY_BROKER_URL = f"amqp://{RABBITMQ_HOST}:{RABBITMQ_PORT}"
-CELERY_BACKEND_URL = f"rpc://{RABBITMQ_HOST}:{RABBITMQ_PORT}"
+CELERY_BACKEND_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}"
 
 app = Celery('indexer',
              broker=CELERY_BROKER_URL,
