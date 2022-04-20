070bb968fde9de614ea6253e87924b8603348483
k-dimentional tree <98183693+kdimentionaltree@users.noreply.github.com>
2022-04-21 00:11:06 +0300

    Indexer http api (#2)

---
+++ indexer/web.py
@@ -0,0 +1,60 @@
+import logging
+import os, sys
+
+from fastapi import FastAPI
+from fastapi import Query
+
+from indexer.database import Block, Transaction, Message, get_session
+from dataclasses import asdict, is_dataclass
+from sqlalchemy import and_
+from sqlalchemy.orm import joinedload
+
+
+logging.basicConfig(format='%(asctime)s %(module)-15s %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+description = ''
+app = FastAPI(
+    title="TONdex",
+    description=description,
+    version='0.1.0',
+    docs_url='/',
+    # responses={
+    #     422: {'description': 'Validation Error'},
+    #     504: {'description': 'Lite Server Timeout'}
+    # },
+    # root_path='/api/v2',
+)
+
+
+@app.on_event("startup")
+def startup():
+    logger.info('Service started successfully')
+
+@app.get('/getTransactionsByMasterchainSeqno')
+def getTransactionsByMasterchainSeqno(
+    seqno: int = Query(..., description="Masterchain seqno"),
+    return_message_bodies: bool = Query(False, description="Return message bodies")
+    ):
+    Session = get_session()
+    
+    txs = []
+    with Session(expire_on_commit=True) as session:
+        block = session.query(Block).filter(and_(Block.seqno == seqno, Block.workchain == -1)).first()
+        block_ids = [block.block_id] + [x.block_id for x in block.shards]
+        if return_message_bodies:
+            txs = session.query(Transaction) \
+                        .filter(Transaction.block_id.in_(block_ids)) \
+                        .options(joinedload(Transaction.in_msg).joinedload(Message.content)) \
+                        .options(joinedload(Transaction.out_msgs).joinedload(Message.content)) \
+                        .all()
+        else:
+            txs = session.query(Transaction) \
+                        .filter(Transaction.block_id.in_(block_ids)) \
+                        .options(joinedload(Transaction.in_msg)) \
+                        .options(joinedload(Transaction.out_msgs)) \
+                        .all()
+
+    return [tx.asdict() for tx in txs]
--- tApi/tonlib/sync_client.py
+++ tApi/tonlib/sync_client.py
@@ -45,59 +45,65 @@ class TonlibClientSync:
     def raw_get_transactions(self, account_address: str, from_transaction_lt: str, from_transaction_hash: str):
         return self.thread.run_coro(self.client_impl.raw_get_transactions(account_address, from_transaction_lt, from_transaction_hash))
 
-    async def raw_get_account_state(self, address: str):
+    def raw_get_account_state(self, address: str):
         return self.thread.run_coro(self.client_impl.raw_get_account_state(address))
 
-    async def generic_get_account_state(self, address: str):
+    def generic_get_account_state(self, address: str):
         return self.thread.run_coro(self.client_impl.generic_get_account_state(address))
 
-    async def _load_contract(self, address):
+    def _load_contract(self, address):
         return self.thread.run_coro(self.client_impl._load_contract(address))
 
-    async def raw_run_method(self, address, method, stack_data, output_layout=None):
+    def raw_run_method(self, address, method, stack_data, output_layout=None):
         return self.thread.run_coro(self.client_impl.raw_run_method(address, method, stack_data, output_layout))
 
-    async def raw_send_message(self, serialized_boc):
+    def raw_send_message(self, serialized_boc):
         return self.thread.run_coro(self.client_impl.raw_send_message(serialized_boc))
 
-    async def _raw_create_query(self, destination, body, init_code=b'', init_data=b''):
+    def _raw_create_query(self, destination, body, init_code=b'', init_data=b''):
         return self.thread.run_coro(self.client_impl._raw_create_query(destination, body, init_code, init_data))
 
-    async def _raw_send_query(self, query_info):
+    def _raw_send_query(self, query_info):
         return self.thread.run_coro(self.client_impl._raw_send_query(query_info))
         
-    async def raw_create_and_send_query(self, destination, body, init_code=b'', init_data=b''):
+    def raw_create_and_send_query(self, destination, body, init_code=b'', init_data=b''):
         return self.thread.run_coro(self.client_impl.raw_create_and_send_query(destination, body, init_code, init_data))
 
-    async def raw_create_and_send_message(self, destination, body, initial_account_state=b''):
+    def raw_create_and_send_message(self, destination, body, initial_account_state=b''):
         return self.thread.run_coro(self.client_impl.raw_create_and_send_message(destination, body, initial_account_state))
 
-    async def raw_estimate_fees(self, destination, body, init_code=b'', init_data=b'', ignore_chksig=True):
+    def raw_estimate_fees(self, destination, body, init_code=b'', init_data=b'', ignore_chksig=True):
         return self.thread.run_coro(self.client_impl.raw_estimate_fees(destination, body, init_code, init_data, ignore_chksig))
 
-    async def raw_getBlockTransactions(self, fullblock, count, after_tx):
+    def raw_getBlockTransactions(self, fullblock, count, after_tx):
         return self.thread.run_coro(self.client_impl.raw_getBlockTransactions(fullblock, count, after_tx))
 
-    async def getTransactions(self, account_address, from_transaction_lt=None, from_transaction_hash=None, to_transaction_lt=0, limit=10):
+    def raw_getBlockTransactionsExt(self, fullblock, count, after_tx):
+        return self.thread.run_coro(self.client_impl.raw_getBlockTransactionsExt(fullblock, count, after_tx))
+
+    def getTransactions(self, account_address, from_transaction_lt=None, from_transaction_hash=None, to_transaction_lt=0, limit=10):
         return self.thread.run_coro(self.client_impl.getTransactions(account_address, from_transaction_lt, from_transaction_hash, to_transaction_lt, limit))
 
-    async def getMasterchainInfo(self):
+    def getMasterchainInfo(self):
         return self.thread.run_coro(self.client_impl.getMasterchainInfo())
 
-    async def lookupBlock(self, workchain, shard, seqno=None, lt=None, unixtime=None):
+    def lookupBlock(self, workchain, shard, seqno=None, lt=None, unixtime=None):
         return self.thread.run_coro(self.client_impl.lookupBlock(workchain, shard, seqno, lt, unixtime))
 
-    async def getShards(self, master_seqno=None, lt=None, unixtime=None):
+    def getShards(self, master_seqno=None, lt=None, unixtime=None):
         return self.thread.run_coro(self.client_impl.getShards(master_seqno, lt, unixtime))
 
-    async def getBlockTransactions(self, workchain, shard, seqno, count, root_hash=None, file_hash=None, after_lt=None, after_hash=None):
+    def getBlockTransactions(self, workchain, shard, seqno, count, root_hash=None, file_hash=None, after_lt=None, after_hash=None):
         return self.thread.run_coro(self.client_impl.getBlockTransactions(workchain, shard, seqno, count, root_hash, file_hash, after_lt, after_hash))
 
-    async def getBlockHeader(self, workchain, shard, seqno, root_hash=None, file_hash=None):
+    def getBlockTransactionsExt(self, workchain, shard, seqno, count, root_hash=None, file_hash=None, after_lt=None, after_hash=None):
+        return self.thread.run_coro(self.client_impl.getBlockTransactionsExt(workchain, shard, seqno, count, root_hash, file_hash, after_lt, after_hash))
+
+    def getBlockHeader(self, workchain, shard, seqno, root_hash=None, file_hash=None):
         return self.thread.run_coro(self.client_impl.getBlockHeader(workchain, shard, seqno, root_hash, file_hash))
 
-    async def tryLocateTxByIncomingMessage(self, source, destination, creation_lt):
+    def tryLocateTxByIncomingMessage(self, source, destination, creation_lt):
         return self.thread.run_coro(self.client_impl.tryLocateTxByIncomingMessage(source, destination, creation_lt))
 
-    async def tryLocateTxByOutcomingMessage(self, source, destination, creation_lt):
+    def tryLocateTxByOutcomingMessage(self, source, destination, creation_lt):
         return self.thread.run_coro(self.client_impl.tryLocateTxByOutcomingMessage(source, destination, creation_lt))
