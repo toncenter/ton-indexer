b8c51c8337be7d5b933296355510c00a6ec81a5c
ms <dungeon666master@protonmail.com>
2023-03-05 14:51:20 +0100

    secondary

--- tondb-scanner/src/main.cpp
+++ tondb-scanner/src/main.cpp
@@ -17,35 +17,25 @@ class DbScanner: public td::actor::Actor {
 private:
   td::actor::ActorOwn<ton::validator::ValidatorManagerInterface> validator_manager_;
 
-  std::string db_root_ = "/zpool/ton-work-testnet/db";
-  td::Ref<ton::validator::ValidatorManagerOptions> opts_;
-  ton::BlockIdExt shard_top_block_id_;
-  ton::ShardIdFull shard_{ton::masterchainId, ton::shardIdAll};
+  std::string db_root_;
+  td::actor::ActorOwn<ton::validator::RootDb> db_;
 
 public:
+
   void set_db_root(std::string db_root) {
     db_root_ = db_root;
   }
 
-  // td::Status create_validator_options() {
-  //   opts_ = ton::validator::ValidatorManagerOptions::create(
-  //     ton::BlockIdExt{ton::masterchainId, ton::shardIdAll, 0, ton::RootHash::zero(), ton::FileHash::zero()},
-  //     ton::BlockIdExt{ton::masterchainId, ton::shardIdAll, 0, ton::RootHash::zero(), ton::FileHash::zero()});
-  //   return td::Status::OK();
-  // }
+  void start_up() override {
+    alarm_timestamp() = td::Timestamp::in(3.0);
+  }
 
   void run() {
     std::cout << "Running!" << std::endl;
-
-    auto top_block_str = td::Slice("(-1,8000000000000000,7002925):AB657E913624D4C1E3316E6CC0BBCB24CEC6868F7532314B99EE9C7DE5438A45:F2788947E2A453863BC213ACAE48DA7904C8F2610731B5BB25194494A6C09FE2");
-    bool status = block::parse_block_id_ext(top_block_str, shard_top_block_id_);
-    if (!status) {
-      std::cout << "ERROR parsing block" << std::endl;
-    }
     
-    auto db = td::actor::create_actor<ton::validator::RootDb>("db", td::actor::ActorId<ton::validator::ValidatorManager>(), db_root_);
+    db_ = td::actor::create_actor<ton::validator::RootDb>("db", td::actor::ActorId<ton::validator::ValidatorManager>(), db_root_);
 
-    auto P = td::PromiseCreator::lambda([SelfId = actor_id(this)](td::Result<BlockHandle> R) {
+    auto P = td::PromiseCreator::lambda([SelfId = actor_id(this)](td::Result<ConstBlockHandle> R) {
       if (R.is_error()) {
         LOG(ERROR) << R.move_as_error();
       } else {
@@ -54,7 +44,34 @@ public:
       }
     });
 
-    td::actor::send_closure(db, &RootDb::get_block_handle, shard_top_block_id_, std::move(P));
+    td::actor::send_closure(db_, &RootDb::get_block_by_seqno, ton::AccountIdPrefixFull(ton::masterchainId, ton::shardIdAll), 7139009, std::move(P));
+  }
+
+  void alarm() override {
+    alarm_timestamp() = td::Timestamp::in(3.0);
+    if (db_.empty()) {
+      return;
+    }
+
+    auto P = td::PromiseCreator::lambda([SelfId = actor_id(this)](td::Result<int> R) {
+      if (R.is_error()) {
+        LOG(ERROR) << R.move_as_error();
+      } else {
+        auto k = R.move_as_ok();
+        LOG(INFO) << "Max masterchain seqno: " << k;
+      }
+    });
+
+    td::actor::send_closure(db_, &RootDb::get_max_masterchain_seqno, std::move(P));
+
+    auto R = td::PromiseCreator::lambda([SelfId = actor_id(this)](td::Result<td::Unit> R) {
+      if (R.is_error()) {
+        LOG(ERROR) << R.move_as_error();
+      } else {
+        LOG(INFO) << "Catch up with primary successful";
+      }
+    });
+    td::actor::send_closure(db_, &RootDb::try_catch_up_with_primary, std::move(R));
   }
 };
 
@@ -79,9 +96,7 @@ int main(int argc, char *argv[]) {
 
   p.add_option('D', "db", "root for dbs",
                [&](td::Slice fname) { td::actor::send_closure(scanner, &DbScanner::set_db_root, fname.str()); });
-  // p.add_option('C', "config", "global config path",
-  //              [&](td::Slice fname) { td::actor::send_closure(x, [&](std::string fname) { std::cout << fname << std::endl; }, fname.str()); });
-  
+
   td::actor::Scheduler scheduler({1});
   scheduler.run_in_context([&] { scanner = td::actor::create_actor<DbScanner>("scanner"); });
   scheduler.run_in_context([&] { p.run(argc, argv).ensure(); });
