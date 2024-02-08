52a9f395f8573f55aa577c92ca020d171613d78a
ms <dungeon666master@protonmail.com>
2024-02-08 19:35:54 +0100

    fix swagger paths

--- indexer/indexer/api/main.py
+++ indexer/indexer/api/main.py
@@ -74,7 +74,7 @@ async def custom_swagger_ui_html():
         openapi_url=app.openapi_url,
         title=app.title + " - Swagger UI",
         oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
-        swagger_js_url="/static/swagger-ui-bundle.js",
-        swagger_css_url="/static/swagger-ui.css",
+        swagger_js_url=settings.api_root_path + "/static/swagger-ui-bundle.js",
+        swagger_css_url=settings.api_root_path+ "/static/swagger-ui.css",
         swagger_favicon_url=''
     )
