1f949b6a8e949d74c50f55527c24a1014fdadf96
verdigos <chdm.ps@gmail.com>
2024-09-10 13:32:35 +0300

    Fix run_event_classifier.sh

--- run_event_classifier.sh
+++ run_event_classifier.sh
@@ -2,19 +2,19 @@
 set -e
 
 if [[ ! $# -eq "3" ]]; then
-    echo "Exactly 3 arguments required: container name, PostgreSQL and MongoDB connection string."
+    echo "Exactly 3 arguments required: container name, PostgreSQL and Redis connection string."
     exit 1
 fi
 
 CONTAINER_NAME=${1}
 PG_DSN=${2}
-MONGO_DSN=${3}
+REDIS_DSN=${3}
 
 docker build -t ton-index-event-classifier:devel -f indexer/classifier.Dockerfile indexer
 
 docker rm --force ${CONTAINER_NAME}
 docker run -e TON_INDEXER_PG_DSN="$PG_DSN" \
-           -e TON_INDEXER_MONGO_DSN="$MONGO_DSN" \
+           -e TON_REDIS_MONGO_DSN="REDIS_DSN" \
            -e TQDM_NCOLS=0 \
            -e TQDM_POSITION=-1 \
            --restart unless-stopped \
