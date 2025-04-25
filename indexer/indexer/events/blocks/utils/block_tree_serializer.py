176cf6159e1043e5dba3a0b88fd4bbebeb00a151
Dmitry Churkin <chdm.ps@gmail.com>
2025-04-25 20:04:26 +0300

    Do not save child actions for contract deployments

--- indexer/indexer/events/blocks/utils/block_tree_serializer.py
+++ indexer/indexer/events/blocks/utils/block_tree_serializer.py
@@ -510,6 +510,7 @@ basic_ops = [ 'call_contract',
               'nft_transfer',
               'nft_mint',
               'jetton_mint',
+              'ton_transfer',
               ]
 
 def serialize_blocks(blocks: list[Block], trace_id, trace: Trace = None, parent_acton_id = None, serialize_child_actions=True) -> tuple[list[Action], str]:
@@ -534,6 +535,8 @@ def serialize_blocks(blocks: list[Block], trace_id, trace: Trace = None, parent_
                 if block.btype not in basic_ops:
                     child_actions, child_state = serialize_blocks(block.children_blocks, trace_id, trace, action.action_id, serialize_child_actions)
                     for child_action in child_actions:
+                        if child_action.type == 'contract_deploy':
+                            continue
                         if child_action.ancestor_type is None:
                             child_action.ancestor_type = []
                         child_action.ancestor_type.append(block.btype)
