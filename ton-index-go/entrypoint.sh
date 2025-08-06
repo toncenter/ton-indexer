ab2b487b891db2cb052203bceb90066d75038b9a
Marat S <dungeon666master@protonmail.com>
2025-08-06 23:50:06 +0000

    pass postgres password in .pgpass file

--- ton-index-go/entrypoint.sh
+++ ton-index-go/entrypoint.sh
@@ -1,29 +1,12 @@
 #!/bin/bash
 set -e
 
-# postgres password
-if [ ! -z "$POSTGRES_PASSWORD_FILE" ]; then
-    echo "Postgres password file: ${POSTGRES_PASSWORD_FILE}"
-    if [ ! -f "${POSTGRES_PASSWORD_FILE}" ]; then
-        echo "Password file not found"
-        exit 1
-    fi
-    POSTGRES_PASSWORD=$(cat ${POSTGRES_PASSWORD_FILE})
-elif [ ! -z "$POSTGRES_PASSWORD" ]; then
-    echo "Postgres password specified"
-else
-    echo "Postgres password file not specified!"
-    exit 1
-fi
-
-if [ "$POSTGRES_PASSWORD" -eq 0 ]; then
-    echo "Using postgres connection without password"
-    export TON_INDEXER_PG_DSN="postgresql://${POSTGRES_USER}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DBNAME}"
-else
-    echo "Using postgres connection with password"
-    export TON_INDEXER_PG_DSN="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DBNAME}"
-fi
-printenv
+# prepare pgpass file
+PW="$(tr -d '\r\n' < /run/secrets/postgres_password)"
+tmp="$(mktemp)"
+printf '*:*:*:*:%s\n' "$PW" > "$tmp"
+chmod 0600 "$tmp"
+export PGPASSFILE="$tmp"
 
 INDEX_ARGS=""
 case $TON_INDEXER_IS_TESTNET in 
@@ -40,4 +23,4 @@ fi
 
 echo "Args: $INDEX_ARGS"
 
-ton-index-go -pg $TON_INDEXER_PG_DSN -bind ":8081" $INDEX_ARGS $@
+ton-index-go -pg "postgresql://${POSTGRES_USER}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DBNAME}" -bind ":8081" $INDEX_ARGS $@
--- ton-index-worker/scripts/entrypoint.sh
+++ ton-index-worker/scripts/entrypoint.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 set -e
 
-# postgres password
-if [ ! -z "$POSTGRES_PASSWORD_FILE" ]; then
-    echo "Postgres password file: ${POSTGRES_PASSWORD_FILE}"
-    if [ ! -f "${POSTGRES_PASSWORD_FILE}" ]; then
-        echo "Password file specified, but not found"
-        exit 1
-    fi
-    POSTGRES_PASSWORD=$(cat ${POSTGRES_PASSWORD_FILE})
-elif [ ! -z "$POSTGRES_PASSWORD" ]; then
-    echo "Postgres password specified"
-else
-    echo "Warning: postgres password file not specified!"
-fi
-
-export PGPASSWORD=$POSTGRES_PASSWORD
+# prepare pgpass file
+PW="$(tr -d '\r\n' < /run/secrets/postgres_password)"
+tmp="$(mktemp)"
+printf '*:*:*:*:%s\n' "$PW" > "$tmp"
+chmod 0600 "$tmp"
+export PGPASSFILE="$tmp"
 
 ulimit -n 1000000
-printenv
-echo "Running binary ${TON_WORKER_BINARY}"
-${TON_WORKER_BINARY} $@
+echo "Running command: $@"
+exec "$@"
