f761b5f659bd34a26368101a391811b9bd9d65cf
Marat <98183742+dungeon-master-666@users.noreply.github.com>
2025-08-07 15:39:57 +0200

    Pass postgres password in .pgpass file (#252)
    * pass postgres password in .pgpass file
    
    * fixes

--- ton-index-worker/scripts/entrypoint.sh
+++ ton-index-worker/scripts/entrypoint.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 set -e
 
-# postgres password
-if [ ! -z "$POSTGRES_PASSWORD_FILE" ]; then
-    echo "Postgres password file: ${POSTGRES_PASSWORD_FILE}"
-    if [ ! -f "${POSTGRES_PASSWORD_FILE}" ]; then
-        echo "Password file specified, but not found"
-        exit 1
-    fi
-    POSTGRES_PASSWORD=$(cat ${POSTGRES_PASSWORD_FILE})
-elif [ ! -z "$POSTGRES_PASSWORD" ]; then
-    echo "Postgres password specified"
-else
-    echo "Warning: postgres password file not specified!"
+# prepare pgpass file
+if [ ! -r /run/secrets/postgres_password ]; then
+    echo "Error: /run/secrets/postgres_password does not exist or is not readable." >&2  
+    exit 1          
 fi
-
-export PGPASSWORD=$POSTGRES_PASSWORD
+PW="$(tr -d '\r\n' < /run/secrets/postgres_password)"
+tmp="$(mktemp)"
+printf '*:*:*:*:%s\n' "$PW" > "$tmp"
+chmod 0600 "$tmp"
+export PGPASSFILE="$tmp"
 
 ulimit -n 1000000
-printenv
-echo "Running binary ${TON_WORKER_BINARY}"
-${TON_WORKER_BINARY} $@
+echo "Running command: $@"
+exec "$@"
