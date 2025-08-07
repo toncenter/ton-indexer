07dc87fc190b9145225e540f872561e3695abe02
Marat S <dungeon666master@protonmail.com>
2025-08-07 00:02:08 +0000

    fixes

--- ton-index-worker/scripts/entrypoint.sh
+++ ton-index-worker/scripts/entrypoint.sh
@@ -2,6 +2,10 @@
 set -e
 
 # prepare pgpass file
+if [ ! -r /run/secrets/postgres_password ]; then
+    echo "Error: /run/secrets/postgres_password does not exist or is not readable." >&2  
+    exit 1          
+fi
 PW="$(tr -d '\r\n' < /run/secrets/postgres_password)"
 tmp="$(mktemp)"
 printf '*:*:*:*:%s\n' "$PW" > "$tmp"
