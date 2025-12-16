f4e35836f5e5bca340177810d2c5ca8f4795b799
Victor Tec <victor@ton.org>
2025-12-16 18:53:46 +0300

    Version.h 1.2.6

--- ton-index-worker/ton-index-postgres/src/version.h
+++ ton-index-worker/ton-index-postgres/src/version.h
@@ -24,7 +24,7 @@ struct Version {
 };
 
 // Defines the latest version of the database schema.
-const Version latest_version{1, 2, 5};
+const Version latest_version{1, 2, 6};
 
 std::optional<Version> get_current_db_version(const std::string& connection_string) {
   try {
