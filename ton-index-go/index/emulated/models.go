429465d0aff6d55d1cd121f40ce96cc82385a3bc
Marat S <dungeon666master@protonmail.com>
2026-01-17 09:29:11 +0000

    temp map signed to confirmed

--- ton-index-go/index/emulated/models.go
+++ ton-index-go/index/emulated/models.go
@@ -624,7 +624,7 @@ func (fs FinalityState) String() string {
 	case FinalityStateConfirmed:
 		return "confirmed"
 	case FinalityStateSigned:
-		return "signed"
+		return "confirmed" // temporary mapping signed to confirmed
 	case FinalityStateFinalized:
 		return "finalized"
 	default:
