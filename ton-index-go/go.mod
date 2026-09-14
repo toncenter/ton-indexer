533cf82859922b62e344d891b2212b0323c94538
Victor Tec <victor@ton.org>
2026-09-14 23:50:40 +0300

    Run the Go ABI generator as a go.mod tool instead of go generate

--- ton-index-go/go.mod
+++ ton-index-go/go.mod
@@ -47,3 +47,5 @@ require (
 	golang.org/x/tools v0.36.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
+
+tool github.com/ton-blockchain/tolk-abi-to-go/cmd/tolk-abi-to-go
--- ton-index-go/index/acton/catalog/generate.go
+++
@@ -1,4 +0,0 @@
-// Package catalog contains the offline Acton ABI catalog and native Go bindings.
-package catalog
-
-//go:generate go run github.com/ton-blockchain/tolk-abi-to-go/cmd/tolk-abi-to-go --catalog catalog.json --output-dir . --package catalog
